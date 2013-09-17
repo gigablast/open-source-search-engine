afd1b3a9a21d2f34c957a0e9a29d527a97b59793
mwells <mwells@sp8.(none)>
2013-09-16 21:42:48 -0600

    added Diffbot.h

---
+++ Diffbot.h
@@ -0,0 +1,10 @@
+
+#ifndef DIFFBOT_H
+#define DIFFBOT_H
+
+bool printCrawlBotPage ( TcpSocket *s , 
+			 HttpRequest *hr ,
+			 SafeBuf *injectionResponse = NULL ) ;
+bool handleDiffbotRequest ( TcpSocket *s , HttpRequest *hr ) ;
+
+#endif
