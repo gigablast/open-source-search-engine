167d2dc99ff6cbccf5d8c6af6fc7b8325ad3e60a
Matt Wells <mwells@trinity.(none)>
2014-01-16 13:40:27 -0800

    nothing.

--- Spider.cpp
+++ Spider.cpp
@@ -10809,7 +10809,7 @@ void updateAllCrawlInfosSleepWrapper ( int fd , void *state ) {
 		Host *h = g_hostdb.getHost(i);
 		// skip if dead
 		if ( g_hostdb.isDead(i) ) {
-			//if ( g_conf.m_logDebugSpider )
+			if ( g_conf.m_logDebugSpider )
 				log("spider: skipping dead host #%li "
 				    "when getting "
 				    "crawl info",i);
