d2f0824f8b7333a0d920e011d5596ceb3137ffc7
mwells <mwells@mwellsvb.(none)>
2014-05-10 16:54:56 -0700

    ah screw it

--- Parms.cpp
+++ Parms.cpp
@@ -10075,8 +10075,7 @@ void Parms::init ( ) {
 	m->m_cgi   = "mit";
 	m->m_off   = (char *)&cr.m_makeImageThumbnails - x;
 	m->m_type  = TYPE_BOOL;
-	// default off until working better
-	m->m_def   = "0";
+	m->m_def   = "1";
 	m++;
 
 	m->m_title = "index spider replies";
