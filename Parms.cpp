1586c2dcd7a09b8ea2a227edd1d93aeaa792e040
mwells <mwells@mwells-VirtualBox.(none)>
2014-07-03 07:57:08 -0700

    minor parm change to back what it was

--- Parms.cpp
+++ Parms.cpp
@@ -13924,7 +13924,7 @@ void Parms::init ( ) {
 	m->m_title = "FORCE DELETE";
 	m->m_desc  = "Check this checkbox to delete the results, not just "
 		"reindex them.";
-	m->m_cgi   = "fdel";
+	m->m_cgi   = "forcedel";
 	m->m_off   = (char *)&gr.m_forceDel - (char *)&gr;
 	m->m_type  = TYPE_CHECKBOX;
 	m->m_page  = PAGE_REINDEX;
