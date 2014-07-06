e4f43848d5c65bd8db2109c9236931c6576a907b
mwells <mwells@mwells-VirtualBox.(none)>
2014-07-06 08:31:27 -0700

    fix http://abc.com in sitelist some more.

--- PageBasic.cpp
+++ PageBasic.cpp
@@ -63,7 +63,7 @@ public:
 	// hash of the subdomain or domain for this line in sitelist
 	long m_thingHash32;
 	// ptr to the line in CollectionRec::m_siteListBuf
-	char *m_patternStr;
+	long m_patternStrOff;
 	// offset of the url path in the pattern, 0 means none
 	short m_pathOff; 
 	short m_pathLen;
@@ -313,7 +313,10 @@ bool updateSiteListBuf ( collnum_t collnum ,
 		pd.m_thingHash32 = u.getHostHash32();
 		// . ptr to the line in CollectionRec::m_siteListBuf. 
 		// . includes pointing to "exact:" too i guess and tag: later.
-		pd.m_patternStr = start;
+		// . store offset since CommandUpdateSiteList() passes us
+		//   a temp buf that will be freed before copying the buf
+		//   over to its permanent place at cr->m_siteListBuf
+		pd.m_patternStrOff = start - siteListArg;
 		// offset of the url path in the pattern, 0 means none
 		pd.m_pathOff = 0;
 		// scan url pattern, it should start at "s"
@@ -430,19 +433,23 @@ char *getMatchingUrlPattern ( SpiderColl *sc , SpiderRequest *sreq ) {
 	// we handle.
 	long slot = dt->getSlot ( &sreq->m_domHash32 );
 
+	char *buf = cr->m_siteListBuf.getBufStart();
+
 	// loop over all the patterns that contain this domain and see
 	// the first one we match, and if we match a negative one.
 	for ( ; slot >= 0 ; slot = dt->getNextSlot(slot,&sreq->m_domHash32)) {
 		// get pattern
 		PatternData *pd = (PatternData *)dt->getValueFromSlot ( slot );
+		// point to string
+		char *patternStr = buf + pd->m_patternStrOff;
 		// is it negative? return NULL if so so url will be ignored
-		//if ( pd->m_patternStr[0] == '-' ) 
+		//if ( patternStr[0] == '-' ) 
 		//	return NULL;
 		// otherwise, it has a path. skip if we don't match path ptrn
 		if ( pd->m_pathOff ) {
 			if ( ! myPath ) myPath = sreq->getUrlPath();
 			if ( strncmp (myPath,
-				      pd->m_patternStr + pd->m_pathOff,
+				      patternStr + pd->m_pathOff,
 				      pd->m_pathLen ) )
 				continue;
 		}
@@ -450,27 +457,27 @@ char *getMatchingUrlPattern ( SpiderColl *sc , SpiderRequest *sreq ) {
 		// for entries like http://domain.com/ we have to match
 		// protocol and url can NOT be like www.domain.com to match.
 		// this is really like a regex like ^http://xyz.com/poo/boo/
-		if ( (pd->m_patternStr[0]=='h' ||
-		      pd->m_patternStr[0]=='H') &&
-		     ( pd->m_patternStr[1]=='t' ||
-		       pd->m_patternStr[1]=='T' ) &&
-		     ( pd->m_patternStr[2]=='t' ||
-		       pd->m_patternStr[2]=='T' ) &&
-		     ( pd->m_patternStr[3]=='p' ||
-		       pd->m_patternStr[3]=='P' ) ) {
-			char *x = pd->m_patternStr+4;
+		if ( (patternStr[0]=='h' ||
+		      patternStr[0]=='H') &&
+		     ( patternStr[1]=='t' ||
+		       patternStr[1]=='T' ) &&
+		     ( patternStr[2]=='t' ||
+		       patternStr[2]=='T' ) &&
+		     ( patternStr[3]=='p' ||
+		       patternStr[3]=='P' ) ) {
+			char *x = patternStr+4;
 			// is it https:// ?
 			if ( *x == 's' || *x == 'S' ) x++;
 			// watch out for subdomains like http.foo.com
 			if ( *x != ':' ) goto nomatch;
 			// ok, we have to substring match exactly. like 
 			// ^http://xyssds.com/foobar/
-			char *a = pd->m_patternStr;
+			char *a = patternStr;
 			char *b = sreq->m_url;
 			for ( ; ; a++, b++ ) {
 				// stop matching when pattern is exhausted
 				if ( is_wspace_a(*a) || ! *a ) 
-					return pd->m_patternStr;
+					return patternStr;
 				if ( *a != *b ) break;
 			}
 			// we failed to match "pd" so try next line
@@ -481,11 +488,11 @@ char *getMatchingUrlPattern ( SpiderColl *sc , SpiderRequest *sreq ) {
 		// was the line just a domain and not a subdomain?
 		if ( pd->m_thingHash32 == sreq->m_domHash32 )
 			// this will be false if negative pattern i guess
-			return pd->m_patternStr;
+			return patternStr;
 		// was it just a subdomain?
 		if ( pd->m_thingHash32 == sreq->m_hostHash32 )
 			// this will be false if negative pattern i guess
-			return pd->m_patternStr;
+			return patternStr;
 	}
 
 
