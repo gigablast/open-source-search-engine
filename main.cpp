36b82474d9eb3a2ed2ef3a57e4605fd4f44f6f47
Matt Wells <mwells@titan.(none)>
2014-09-18 19:38:32 -0700

    fix infinite loop bug in Query.cpp

--- main.cpp
+++ main.cpp
@@ -4596,6 +4596,9 @@ int install ( install_flag_konst_t installFlag , long hostId , char *dir ,
 	// this is a big rcp so only do one at a time...
 	if  ( installFlag == ifk_install ) maxOut = 1;
 
+	// same with this. takes too long on gk144, jams up
+	if  ( installFlag == ifk_installgb ) maxOut = 1;
+
 	// go through each host
 	for ( long i = 0 ; i < g_hostdb.getNumHosts() ; i++ ) {
 		Host *h2 = g_hostdb.getHost(i);
