4ed8231222f73374983794556c8fd3b5e9f75fbf
Matt Wells <mwells2@gigablast.com>
2015-04-07 09:56:04 -0600

    upp max vfds again

--- File.h
+++ File.h
@@ -22,7 +22,8 @@
 // . max # of VIRTUAL file descriptors
 // . man, chris has 958 files, lets crank it up from 2k to 5k
 // . boost up to 50,000 since we are hitting this limit with crawlbot
-#define MAX_NUM_VFDS (50*1024)
+// . we are hitting again with crawlbot, boost to 200k from 50k
+#define MAX_NUM_VFDS (200*1024)
 
 #include <sys/types.h>       // for open/lseek
 #include <sys/stat.h>        // for open
