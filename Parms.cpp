1dd3912ca05dc4f8ed67efd081b991cf11cd6ae5
mwells <mwells2@gigablast.com>
2015-04-21 08:19:32 -0600

    default isr back on

--- Parms.cpp
+++ Parms.cpp
@@ -17779,7 +17779,8 @@ void Parms::init ( ) {
 	// and we add gbdocspidertime and gbdocindextime terms so you
 	// can use those to sort regular docs and not have spider reply
 	// status docs in the serps.
-	m->m_def   = "0";
+	// back on 4/21/2015 seems pretty stable.
+	m->m_def   = "1";
 	m->m_page  = PAGE_SPIDER;
 	m->m_obj   = OBJ_COLL;
 	m->m_flags = PF_CLONE;
