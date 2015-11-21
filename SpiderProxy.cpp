d55932d0b6cb8d377187065dd4c1703c4f20138c
Matt Wells <mwells2@gigablast.com>
2015-11-21 10:43:23 -0800

    fix spider proxy table bug that seemed to be the reason for the table getting so full. but in case it does get full again added a call the hashtablex::empty() so we don't freeze up any more.

--- SpiderProxy.cpp
+++ SpiderProxy.cpp
@@ -899,8 +899,8 @@ void handleRequest54 ( UdpSlot *udpSlot , int32_t niceness ) {
 	static int32_t s_lbid = 0;
 	// add it now, iff not for passing to diffbot backend
 	if ( preq->m_opCode != OP_GETPROXYFORDIFFBOT ) {
-		s_loadTable.addKey ( &urlIp , &bb );
 		bb.m_id = s_lbid++;
+		s_loadTable.addKey ( &urlIp , &bb );
 		// winner count update
 		winnersp->m_timesUsed++;
 	}
@@ -941,7 +941,7 @@ void handleRequest54 ( UdpSlot *udpSlot , int32_t niceness ) {
 	time_t now = nowms / 1000;
 	if ( s_lastTime == 0 ) s_lastTime = now;
 	time_t elapsed = now - s_lastTime;
-	if ( elapsed > 120 && s_loadTable.getNumSlots() > 20000 ) {
+	if ( elapsed > 120 && s_loadTable.getNumSlots() > 10000 ) {
 		log("sproxy: flushing %i entries from proxy loadtable that "
 		    "have accumulated since %i seconds ago",
 		    (int)s_loadTable.m_numSlotsUsed,(int)elapsed);
@@ -951,8 +951,6 @@ void handleRequest54 ( UdpSlot *udpSlot , int32_t niceness ) {
 	}
 
 
-	/*
-	  MDW: take this out since it was freezing things up
 	int32_t sanityCount = 0;//s_loadTable.getNumSlots();
 	// top:
 	// now remove old entries from the load table. entries that
@@ -982,7 +980,6 @@ void handleRequest54 ( UdpSlot *udpSlot , int32_t niceness ) {
 		//i--;
 		//goto top;
 	}
-	*/
 
 	// send the proxy ip/port/LBid back to user
 	g_udpServer.sendReply_ass ( udpSlot->m_tmpBuf , // msg
